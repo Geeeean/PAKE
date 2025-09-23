2d50a347e63dc7bb6adfe14cc8c359b2869ac990
JakobKjellberg02 <jakob.zornigkjellberg@gmail.com>
2025-09-23 12:22:35 +0200

    Maybe better solution for concatenate

--- src/utils.c
+++ src/utils.c
@@ -7,24 +7,17 @@ int sum(const int a, const int b)
     return a + b;
 }
 
-int setup_keys(char* password, char* id_client, char* id_server) {
-    size_t total_length = strlen(password) + strlen(id_client) + strlen(id_server);
-    unsigned char* buffer = malloc(total_length);
 
-    // Ugly code, will maybe us strcat but idk
-    memcpy(buffer, password, strlen(password));
-    memcpy(buffer + strlen(password), id_client, strlen(id_client));
-    memcpy(buffer + strlen(password) + strlen(id_client), id_server, strlen(id_server));
-    
-    unsigned char phi1[32];
-    unsigned char phi2[32];
-    H_function(buffer, total_length, phi1, phi2);
-    return 0;
-}
+int H_function(char* password, char* id_client, char* id_server, unsigned char output0[32], unsigned char output1[32]) {
+    unsigned char hash[crypto_hash_sha512_BYTES];
+    crypto_hash_sha512_state state;
+    crypto_hash_sha512_init(&state);
 
-int H_function(unsigned char* input, size_t len, unsigned char output0[32], unsigned char output1[32]) {
-    unsigned char hash[64];   
-    crypto_hash_sha512(hash, input, len);
+    crypto_hash_sha512_update(&state, password, strlen(password));
+    crypto_hash_sha512_update(&state, id_client, strlen(id_client));
+    crypto_hash_sha512_update(&state, id_server, strlen(id_server));
+
+    crypto_hash_sha512_final(&state, hash);
     
     // To do - Need converting part
     memcpy(output0, hash, 32);
@@ -33,7 +26,7 @@ int H_function(unsigned char* input, size_t len, unsigned char output0[32], unsi
 }
 
 int H_prime_function(const input, const len, unsigned char out[32]) {
-    unsigned char hash[64];
+    unsigned char hash[crypto_hash_sha512_BYTES];
     crypto_hash_sha512(hash, input, len);
     memcpy(out, hash, 32);
     return 0;
