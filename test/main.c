b6762bd40c5438f176f0d22ed6385ca2729b099a
Geeeean <violoni.gianluca@gmail.com>
2025-10-30 21:29:48 +0100

    solving test warnings

--- test/main.c
+++ test/main.c
@@ -1,19 +1,25 @@
-#include<unity.h>
-#include<utils.h>
 #include "log.h"
 #include <stdio.h>
 #include <string.h>
+#include <unity.h>
+#include <utils.h>
 
-void setUp(void) { sodium_init(); }
+void setUp(void)
+{
+    int result = sodium_init();
+    if (result < 0) {
+        LOG_ERROR("While sodiume init, aborting...");
+        exit(EXIT_FAILURE);
+    }
+}
 
 void tearDown(void) {}
 
 int run_setup_and_key_exchange(const unsigned char *password,
-                                const unsigned char *client_id,
-                                const unsigned char *server_id,
-                                unsigned char key_client[32],
-                                unsigned char key_server[32]) 
-    {
+                               const unsigned char *client_id,
+                               const unsigned char *server_id,
+                               unsigned char key_client[32], unsigned char key_server[32])
+{
     /*** CLIENT PAKE ***/
     // TEMP: Our way of getting the public and fixed group elements a,b
     // Maybe you have a better way Gianluca
@@ -61,7 +67,7 @@ int run_setup_and_key_exchange(const unsigned char *password,
     H_prime(phi0, sizeof(phi0), client_id, strlen((const char *)client_id), server_id,
             strlen((const char *)server_id), u, sizeof(u), v, sizeof(v), w, sizeof(w), d,
             sizeof(d), key_client);
-    
+
     unsigned char a_phi0[crypto_core_ristretto255_BYTES];
     unsigned char u_a_phi0[crypto_core_ristretto255_BYTES];
 
@@ -84,43 +90,42 @@ int run_setup_and_key_exchange(const unsigned char *password,
 
     // Compute session key k
     // k = H′(φ0 ‖ idC ‖ idS ‖ u ‖ v ‖ w ‖ d)
-    if (H_prime(phi0, sizeof(phi0),
-                (const unsigned char *)client_id, strlen(client_id),
-                (const unsigned char *)server_id, strlen(server_id),
-                u, sizeof(u),
-                v, sizeof(v),
-                w, sizeof(w),
-                d, sizeof(d),
-                key_server) != 0) {
+    if (H_prime(phi0, sizeof(phi0), (const unsigned char *)client_id,
+                strlen((char *)client_id), (const unsigned char *)server_id,
+                strlen((char *)server_id), u, sizeof(u), v, sizeof(v), w, sizeof(w), d,
+                sizeof(d), key_server) != 0) {
         LOG_ERROR("Error computing H'");
         return -1;
-    } 
+    }
     return 0;
 }
 
-void simple_protocol_correct(void) {
+void simple_protocol_correct(void)
+{
     unsigned char key_client[32];
     unsigned char key_server[32];
-    run_setup_and_key_exchange((unsigned char*) "password123", (unsigned char*) "name", 
-                               (unsigned char*) "server", key_client, key_server);
+    run_setup_and_key_exchange((unsigned char *)"password123", (unsigned char *)"name",
+                               (unsigned char *)"server", key_client, key_server);
     TEST_ASSERT_EQUAL_UINT8_ARRAY(key_client, key_server, 32);
 }
 
-void protocol_doesnt_produce_same_keys(void) {
+void protocol_doesnt_produce_same_keys(void)
+{
     unsigned char key_client_1[32];
     unsigned char key_server_1[32];
-    run_setup_and_key_exchange((unsigned char*) "password123", (unsigned char*) "name", 
-                               (unsigned char*) "server", key_client_1, key_server_1);
-    
+    run_setup_and_key_exchange((unsigned char *)"password123", (unsigned char *)"name",
+                               (unsigned char *)"server", key_client_1, key_server_1);
+
     unsigned char key_client_2[32];
     unsigned char key_server_2[32];
-    run_setup_and_key_exchange((unsigned char*) "password123", (unsigned char*) "name", 
-                               (unsigned char*) "server", key_client_2, key_server_2);
+    run_setup_and_key_exchange((unsigned char *)"password123", (unsigned char *)"name",
+                               (unsigned char *)"server", key_client_2, key_server_2);
     TEST_ASSERT_TRUE(memcmp(key_client_1, key_client_2, 32) != 0);
     TEST_ASSERT_TRUE(memcmp(key_server_1, key_server_2, 32) != 0);
 }
 
-int main() {
+int main()
+{
     UNITY_BEGIN();
     RUN_TEST(simple_protocol_correct);
     RUN_TEST(protocol_doesnt_produce_same_keys);
